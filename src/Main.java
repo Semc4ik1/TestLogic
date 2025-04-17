3f64979b13e56ef4dc7e4b5ee2d2df2fde5ebff5
Семён <Plohish335@mail.ru>
2025-04-17 22:49:08 +0600

    1. Новый калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Создаем объект класса Logic
+//        urodi
         Logic logic = new Logic();
 
         // Используем метод addNumbers для вычисления суммы чисел
